6db0eadf4deaf0b898bfb35a169f3b9442e61e9b
jorgeamadosoria <darksoul.uci@gmail.com>
2020-02-05 00:32:00 -0600

    finished pie grid and stacked horizontals

--- src/app/talentscape/talentscape.module.ts
+++ src/app/talentscape/talentscape.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewComponent } from './view/view.component';
 import { TalentscapeRoutingModule } from './talentscape-routing.module';
-
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 
 @NgModule({
   declarations: [ViewComponent],
   imports: [
     TalentscapeRoutingModule,
-    CommonModule
+    CommonModule, NgxChartsModule
   ]
 })
 export class TalentscapeModule { }
--- src/app/talentscape/view/view.component.ts
+++ src/app/talentscape/view/view.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Service } from 'src/app/service/service';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-view',
@@ -8,27 +7,31 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent implements OnInit {
-  view: any[] = [700, 400];
-
-  // options
-  showXAxis: boolean = true;
-  showYAxis: boolean = true;
-  gradient: boolean = false;
-  showLegend: boolean = true;
-  showXAxisLabel: boolean = true;
-  yAxisLabel: string = 'Attributes';
-  showYAxisLabel: boolean = true;
-  xAxisLabel: string = 'Levels';
-
   colorScheme = {
-    domain: ['#5AA454', '#C7B42C', '#AAAAAA']
+    domain: ['#7aa3e5', '#5AA454', '#E44D25', '#CFC0BB',  '#a8385d', '#aae3f5']
   };
+
+  // bus number
+  viewBusNumber: any[] = [700, 400];
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  yAxisLabel = 'Attributes';
+  showYAxisLabel = true;
   busNumber = 'controllers/talentscape/busnumber';
   busNumberGraph: any;
+
+  // skill coverage
+  showLabels = true;
+  viewSkillCoverage: any[] = [500, 400];
+  skillCoverage = 'controllers/talentscape/skillcoverage';
+  skillCoverageGraph: any;
+
   constructor(private service: Service) { }
 
   ngOnInit() {
     this.service.stat(this.busNumber).subscribe(data => this.busNumberGraph = data);
+    this.service.stat(this.skillCoverage).subscribe(data => this.skillCoverageGraph = data);
   }
 
 }
