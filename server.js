87b769ceed3d3b6c867e36a28c130185b251e35a
jorgeamadosoria <darksoul.uci@gmail.com>
2020-02-06 23:04:11 -0600

    git ignore, and server.js update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ path = require('path');
 app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/profiler-ui'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/profiler-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
