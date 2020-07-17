f5cb14140ed4ea5d4b2964c89cceac04fb90213f
Jorge Soria <jsoria@NSL-JSORIAs-MacBook-Pro.local>
2020-07-16 21:30:14 -0500

    aesthetic changes to view graphs

--- src/app/talentscape/view/view.component.ts
+++ src/app/talentscape/view/view.component.ts
@@ -8,13 +8,14 @@ import { Service } from 'src/app/service/service';
 })
 export class ViewComponent implements OnInit {
   colorScheme = {
-    domain: ['#7aa3e5', '#5AA454', '#E44D25', '#CFC0BB',  '#a8385d', '#aae3f5']
+    domain: ['#1abc9c', '#7f8c8d', '#f1c40f', '#2c3e50',  '#2ecc71', '#bdc3c7','#e67e22','#8e44ad',
+    '#3498db','#c0392b','#e74c3c','#2980b9','#9b59b6','#d35400','#8e44ad','#ecf0f1','#34495e','#f39c12','#2c3e50','#95a5a6']
   };
 
   // bus number
-  viewBusNumber: any[] = [700, 400];
+  viewBusNumber: any[] = [700, 500];
   showYAxis = true;
-  gradient = false;
+  gradientBusNumber = false;
   showLegend = true;
   yAxisLabel = 'Attributes';
   showYAxisLabel = true;
@@ -23,15 +24,34 @@ export class ViewComponent implements OnInit {
 
   // skill coverage
   showLabels = true;
-  viewSkillCoverage: any[] = [500, 400];
+  viewSkillCoverage: any[] = [600, 500];
   skillCoverage = 'controllers/talentscape/skillcoverage';
   skillCoverageGraph: any;
+  gradientSkillCoverage = false;
+  animations: boolean = true;
+
+  // top most searched
+  topMostSearched = 'controllers/talentscape/topsearched';
+  topMostSearchedTable: any;
+
+
+  // top most endorsed
+  topMostEndorsed = 'controllers/talentscape/mostendorsed';
+  topMostEndorsedTable: any;
+
+
+  // top most learnable
+  topMostLearnable = 'controllers/talentscape/mostlearnable';
+  topMostLearnableTable: any;
 
   constructor(private service: Service) { }
 
   ngOnInit() {
     this.service.stat(this.busNumber).subscribe(data => this.busNumberGraph = data);
     this.service.stat(this.skillCoverage).subscribe(data => this.skillCoverageGraph = data);
+    this.service.stat(this.topMostSearched).subscribe(data => this.topMostSearchedTable = data);
+    this.service.stat(this.topMostEndorsed).subscribe(data => this.topMostEndorsedTable = data);
+    this.service.stat(this.topMostLearnable).subscribe(data => this.topMostLearnableTable = data);
   }
 
 }
